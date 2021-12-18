6f6bbe215c4dcc57fb0372c8f82527babf5e7ccb
fengqihang <18302010037@fudan.edu.cn>
2021-12-18 18:13:26 +0800

    second

--- src/main/java/com/example/project/respository/AppointmentRepository.java
+++ src/main/java/com/example/project/respository/AppointmentRepository.java
@@ -1,11 +1,9 @@
 package com.example.project.respository;
 
-import com.example.project.entity.User;
+import com.example.project.entity.Appointment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User,Integer> {
-      User findByUsername(String name);
-
+public interface AppointmentRepository extends CrudRepository<Appointment,Integer> {
 }
--- src/main/java/com/example/project/respository/CarRepository.java
+++ src/main/java/com/example/project/respository/CarRepository.java
@@ -1,11 +1,13 @@
 package com.example.project.respository;
 
+import com.example.project.entity.Car;
 import com.example.project.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User,Integer> {
-      User findByUsername(String name);
+public interface CarRepository extends CrudRepository<Car,Integer> {
+
+
 
 }
