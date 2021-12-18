7f868a9a1f47891fdf0dac7ba2081a26b35d76ed
fengqihang <18302010037@fudan.edu.cn>
2021-12-18 22:56:54 +0800

    second

---
+++ src/main/java/com/example/project/controller/GarageController.java
@@ -0,0 +1,16 @@
+package com.example.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GarageController {
+
+    @RequestMapping(value = "/addGarage")
+    public void addGarage(){
+
+    }
+
+
+
+}
